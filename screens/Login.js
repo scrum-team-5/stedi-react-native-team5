a4d79c29719ebf68abe4e311c6f7054902b9dde1
Pun <punyanith.t@gmail.com>
2023-05-16 11:11:26 -0600

    buttons

--- screens/Login.js
+++ screens/Login.js
@@ -8,6 +8,12 @@ export default function Login(props){
             <Button title="Log In" onPress={()=>props.setUserLoggedIn(true)}></Button>
             <Text>Hello Ryan</Text>
             <Button title="ryanButton" onPress={()=>console.log("Hello Ryan")}></Button>
+
+            <Text>Hello Samantha</Text>
+            <Button title="SamanthaButton" onPress={()=>console.log("Hello Samantha")}></Button>
+
+            <Text>Brian's Button</Text>
+            <Button title="BrianButton" onPress= {console.log("Hello Brian")}></Button>
         </View>
 
     );
