4c0274c42d9ad38cdced67f5212a60654e0cd4ea
cris-rr <59718617+cris-rr@users.noreply.github.com>
2023-05-17 21:59:32 -0500

    cristhians button

--- screens/Login.js
+++ screens/Login.js
@@ -1,34 +1,44 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
-export default function Login(props){
-    return(
-        <View style={styles.login}>
-            <Text>This is the Login Screen</Text>
-            
-            <Button title="Seth Button" onPress={()=>console.log("Hello Seth!")}></Button>
-            
-            <Button title="Matias Button" onPress={()=>console.log("Hello Matias!")}></Button>
-            
-            <Button title="Alejandro Button" onPress={()=>console.log("Hello Alejandro!")}></Button>
+export default function Login(props) {
+  return (
+    <View style={styles.login}>
+      <Text>This is the Login Screen</Text>
 
-        </View>
-        
+      <Button
+        title="Seth Button"
+        onPress={() => console.log('Hello Seth!')}
+      ></Button>
 
-    );
+      <Button
+        title="Matias Button"
+        onPress={() => console.log('Hello Matias!')}
+      ></Button>
+
+      <Button
+        title="Alejandro Button"
+        onPress={() => console.log('Hello Alejandro!')}
+      ></Button>
+
+      <Button
+        title="Cristhians Button"
+        onPress={() => console.log('Hello Cristhians!')}
+      ></Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    login: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        backgroundColor: 'green',
-        height: '12%',
-        alignItems: 'flex-end',
-        paddingBottom: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        
-      },
-})
+  login: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    backgroundColor: 'green',
+    height: '12%',
+    alignItems: 'flex-end',
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10
+  }
+});
